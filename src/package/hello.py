40deabe3e32fd851f504f4280e90215bc4ec6b63
Bellanov <bellanovapilli@gmail.com>
2022-12-16 09:17:56 -0500

    Update hello.py

--- src/package/hello.py
+++ src/package/hello.py
@@ -1,5 +1,15 @@
-"""Simple Python Program."""
+"""Flask Application."""
+from flask import Flask, make_response, request, jsonify
 
 
-def hello_world():
-    return "Hello World!"
+# Create Flask's `app` object
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    my_dict = {'key': 'dictionary value'}
+    headers = {"Content-Type": "application/json"}
+    return make_response(jsonify(my_dict), 200, headers)
